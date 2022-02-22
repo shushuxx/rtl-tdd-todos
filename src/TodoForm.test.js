ffbe20f28d37657a2638941f9d6facacb1b6a10a
shushuxx <wsy0912@gmail.com>
2022-02-22 02:47:26 +0000

    TodoForm Submit test ok

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -14,4 +14,14 @@ describe('<TodoForm />', () => {
         fireEvent.change(input, {target: {value: 'TDD 배우기'}});
         expect(input).toHaveAttribute('value', 'TDD 배우기');
     })
+    it('calls onInsert and clears input', () => {
+        const onInsert = jest.fn();
+        const {getByText, getByPlaceholderText} = render(<TodoForm onInsert={onInsert} />);
+        const input = getByPlaceholderText('할 일을 입력하세요');
+        const button = getByText('등록');
+        fireEvent.change(input, {target: {value: 'TDD 배우기'}});
+        fireEvent.click(button);
+        expect(onInsert).toBeCalledWith('TDD 배우기');
+        expect(input).toHaveAttribute('value', '');
+    })
 })
