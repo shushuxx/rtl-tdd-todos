16eb3a643f122be165025ce7e9c3029eac8b353a
shushuxx <wsy0912@gmail.com>
2022-02-24 05:42:11 +0000

    Add app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import TodoApp from './TodoApp';
 
-const App = () => {
-  return <TodoApp />;
-
-};
+function App () {
+  return (
+    <div  className="App">
+    <h2>CI CD jenkins with github auto deploy</h2>
+    <TodoApp />
+    </div>
+  );
+}
 
 export default App;
