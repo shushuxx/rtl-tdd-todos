e430d4606ff1f9885fb272a027933be59f6e6855
shushuxx <wsy0912@gmail.com>
2022-03-02 01:59:49 +0000

    test build

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TodoApp from './TodoApp';
 function App () {
   return (
     <div  className="App">
-    <h2>CI CD jenkins with github auto deploy1</h2>
+    <h2>CI CD jenkins with github auto deploy</h2>
     <TodoApp />
     </div>
   );
